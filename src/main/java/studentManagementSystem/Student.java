98b6faa5c2a1a80949fee6dd09cc74d8ee47c3f3
asa <xying5014@gmail.com>
2026-06-03 19:58:25 +0700

    Modify Student

--- src/main/java/studentManagementSystem/Student.java
+++ src/main/java/studentManagementSystem/Student.java
@@ -21,9 +21,6 @@ public class Student {
     public String getUid() {
         return uid;
     }
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
 
     public String getName() {
         return name;
